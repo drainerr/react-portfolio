eb0f670cb4ec1b33ca2091c4d2b9458a6464f4e0
George <george@pop-os.localdomain>
2022-04-18 02:51:13 +0400

    create project card component

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,8 +1,48 @@
+import image from '../../images/wp5591516.webp';
+import styles from './projects.module.css';
 const Projects = () => {
   return (
-    <div>
-      <h1>This is projects page</h1>
-    </div>
+    <ul className={styles.projects}>
+      <h1 className={styles.title}>{`< Projects />`}</h1>
+      <li>
+        <img src={image} alt="ra" className={styles.img}></img>
+        <div className={styles.buttons}>
+          <button>Demo</button>
+          <button>Source Code</button>
+        </div>
+        <div className={styles.details}>
+          <h4>Covid Statistics </h4>
+        </div>
+      </li>
+      <li>
+        <img src={image} alt="ra" className={styles.img}></img>
+        <div className={styles.buttons}>
+          <button>Demo</button>
+          <button>Source Code</button>
+        </div>
+        <div className={styles.details}>
+          <h4>Covid Statistics </h4>
+        </div>
+      </li>
+      <li>
+        <img src={image} alt="ra" className={styles.img}></img>
+      </li>
+      <li>
+        <img src={image} alt="ra" className={styles.img}></img>
+      </li>
+      <li>
+        <img src={image} alt="ra" className={styles.img}></img>
+      </li>
+      <li>
+        <img src={image} alt="ra" className={styles.img}></img>
+      </li>
+      <li>
+        <img src={image} alt="ra" className={styles.img}></img>
+      </li>
+      <li>
+        <img src={image} alt="ra" className={styles.img}></img>
+      </li>
+    </ul>
   );
 };
 
